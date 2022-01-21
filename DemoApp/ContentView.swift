16f283861758eeb0499df49b9cbbcf14635430c4
yelramy <youssef.elramy@gmail.com>
2022-01-21 14:30:46 -0500

    Initial Commit

---
+++ DemoApp/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  DemoApp
+//
+//  Created by Ramy's on 1/21/22.
+//  Copyright © 2022 Ramy's. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
